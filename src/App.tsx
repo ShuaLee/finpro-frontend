87fda35b644fe7f42897bf8d7b8e890a1570f6dc
Josh Grein <engineer@STANMECH.local>
2025-07-03 16:33:08 -0400

    Working on grid.

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,38 @@ import NavBar from "./components/NavBar"
 
 
 function App() {
-  return <Grid templateAreas={{
+  return <Grid 
+    templateAreas={{
     base: `"topbar" "main"`,
     lg: `"nav topbar" "nav main"`,
-  }}>
-    <GridItem area='nav' bg='coral' display={{base: 'none', lg: 'block'}}>
+    }}
+    templateColumns={{base: '1fr', lg: '255px 1fr'}}
+    templateRows={'60px 1fr'}
+    height="100vh"
+  >
+    <GridItem 
+      area="nav" 
+      bg="coral"
+      display={{base: 'none', lg: 'block'}}
+      position="fixed"
+      top="0.75rem"
+      left="0.75rem"
+      width="230px"
+      height="calc(100vh - 1.5rem)"
+      zIndex="1000"
+      borderRadius="2xl"
+      boxShadow="lg"
+      overflow="hidden"
+      >
       <NavBar />
     </GridItem>
-    <GridItem area='topbar' bg='purple'>Nav</GridItem>
-    <GridItem area='main' bg='dodgerblue'>Main</GridItem>
+    <GridItem 
+      area='topbar'
+      bg='purple'
+    >
+      Nav
+    </GridItem>
+    <GridItem area='main' bg='dodgerblue' overflowY="auto">Main</GridItem>
   </Grid>
 }
 
