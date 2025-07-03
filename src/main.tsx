2ad3f3985d0d509eb6b62efeb3b250dd35fdd7c6
Josh Grein <engineer@STANMECH.local>
2025-07-03 15:51:50 -0400

    Capable of dark mode. Dark needs to be adjusted.

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { ColorModeProvider } from "./components/ui/color-mode.tsx"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Provider>
-      <App />
-    </Provider>
+    <ColorModeProvider>
+      <Provider>
+        <App />
+      </Provider>
+    </ColorModeProvider>
   </StrictMode>,
 )
