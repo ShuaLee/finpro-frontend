e672ffc1124703210466ed4d5dd9d653afb55fa1
Josh Grein <engineer@STANMECH.local>
2025-07-03 15:17:40 -0400

    initial creation of dashboard layout.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { Grid, GridItem } from "@chakra-ui/react"
 
 
 function App() {
-  return <div></div>
+  return <Grid templateAreas={{
+    base: `"topbar" "main"`,
+    lg: `"nav topbar" "nav main"`,
+  }}>
+    <GridItem area='nav' bg='coral' display={{base: 'none', lg: 'block'}}>Nav</GridItem>
+    <GridItem area='topbar' bg='purple'>Nav</GridItem>
+    <GridItem area='main' bg='dodgerblue'>Main</GridItem>
+  </Grid>
 }
 
 export default App
